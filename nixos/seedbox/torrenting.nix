342190790a2f1cb4819a3f364036f8aeaacce8f0
David Alexander <opensource@thelonelyghost.com>
2023-09-10 23:57:51 -0400

    Maybe no ipv6 (unless default allowed)?

--- nixos/seedbox/torrenting.nix
+++ nixos/seedbox/torrenting.nix
@@ -244,7 +244,7 @@ in {
           "--cap-add=NET_ADMIN"
           "--cap-add=SYS_MODULE"
           "--sysctl=net.ipv4.conf.all.src_valid_mark=1"
-          "--sysctl=net.ipv6.conf.all.disable_ipv6=0"
+          # "--sysctl=net.ipv6.conf.all.disable_ipv6=0"
         ];
 
         ports = [
